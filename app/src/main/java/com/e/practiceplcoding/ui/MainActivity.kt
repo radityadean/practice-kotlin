345d6793d41270daf81b01d65c49affb82d2dab8
raditya dean <radityadean91@gmail.com>
2021-06-03 11:00:46 +0700

    activity test

--- app/src/main/java/com/e/practiceplcoding/ui/MainActivity.kt
+++ app/src/main/java/com/e/practiceplcoding/ui/MainActivity.kt
@@ -3,25 +3,40 @@ package com.e.practiceplcoding.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.e.practiceplcoding.R
 import com.e.practiceplcoding.db.RunDAO
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    @Inject
-    lateinit var runDAO: RunDAO
+//    @Inject
+//    lateinit var runDAO: RunDAO
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("runDao", "RUNDAO: ${runDAO.hashCode()}")
+//        Log.d("runDao", "RUNDAO: ${runDAO.hashCode()}")
 
+//        val myArray = arrayOf("hello", "guys", "whats up?")
+//        println(myArray[0])
 
-        val myArray = arrayOf("hello", "guys", "whats up?")
-        println(myArray[0])
+        setSupportActionBar(toolbar)
+        bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
+
+        navHostFragment.findNavController()
+            .addOnDestinationChangedListener { _, destination, _ ->
+                when(destination.id) {
+                    R.id.settingsFragment, R.id.runFragment, R.id.statisticsFragment ->
+                        bottomNavigationView.visibility = View.VISIBLE
+                    else -> bottomNavigationView.visibility = View.GONE
+                }
+            }
     }
 
 }
