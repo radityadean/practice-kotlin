744b57ef493b89c042b1d01a7a98f61bcd3e09d8
raditya dean <radityadean91@gmail.com>
2021-06-08 18:14:19 +0700

    saving run to database

--- app/src/main/java/com/e/practiceplcoding/ui/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/e/practiceplcoding/ui/viewmodels/MainViewModel.kt
@@ -2,9 +2,17 @@ package com.e.practiceplcoding.ui.viewmodels
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.e.practiceplcoding.db.Run
 import com.e.practiceplcoding.repositories.MainRepository
+import kotlinx.coroutines.launch
 
 class MainViewModel @ViewModelInject constructor(
     val mainRepository: MainRepository
-): ViewModel() {
+) : ViewModel() {
+
+    fun insertRun(run: Run) = viewModelScope.launch {
+        mainRepository.insertRun(run)
+    }
+
 }
